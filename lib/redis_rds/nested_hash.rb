ba1ee075de04bbad99f86c466809fda0abb8349f
Nicolas Pepin-Perreault <pepin-perreault@checkitmobile.com>
2015-09-21 16:47:43 +0200

    added missing setnx and setex to nested_hash.rb

--- lib/redis_rds/nested_hash.rb
+++ lib/redis_rds/nested_hash.rb
@@ -1,38 +1,38 @@
 module RedisRds
-	class NestedHash < RedisRds::Hash
-		SEPARATOR = ':'
+  class NestedHash < RedisRds::Hash
+    SEPARATOR = ':'
 
-		def get(*keys)
-			super(format_key(keys))
-		end
+    def setnx(*keys, value)
+      return super(format_keys(keys), value)
+    end
 
-		def set(*keys, value)
-			super(format_key(keys), value)
-		end
+    def incrby(*keys, increment)
+      return super(format_keys(keys), increment)
+    end
 
-		def incr(*keys)
-			super(format_key(keys))
-		end
+    def key?(key)
+      return super(format_keys(keys))
+    end
 
-		def decr(*keys)
-			super(format_key(keys))
-		end
+    def get(*keys)
+      super(format_key(keys))
+    end
 
-		def incrby(*keys, increment)
-			super(format_key(keys), increment)
-		end
+    def set(*keys, value)
+      super(format_key(keys), value)
+    end
 
-		def decrby(*keys, increment)
-			super(format_key(keys), increment)
-		end
+    def setex(*keys, value, expiry)
+      super(format_keys(keys), value, expiry)
+    end
 
-		def remove(*keys)
-			super(format_key(keys))
-		end
+    def remove(*keys)
+      super(format_key(keys))
+    end
 
-		def format_key(keys)
-			return Array.wrap(keys).join(SEPARATOR)
-		end
-		private :format_key
-	end
+    def format_key(keys)
+      return Array.wrap(keys).join(SEPARATOR)
+    end
+    private :format_key
+  end
 end
