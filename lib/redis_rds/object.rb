2aed5ef00dc306ef6921c157ee68b9ae24e280cb
Sergio Medina <medina@checkitmobile.com>
2016-05-03 14:37:01 +0200

    lint object.rb

--- lib/redis_rds/object.rb
+++ lib/redis_rds/object.rb
@@ -1,74 +1,74 @@
 module RedisRds
-	class Object
-		attr_accessor :redis_key
+  class Object
+    attr_accessor :redis_key
 
-		def initialize(redis_key)
-			@redis_key = format_redis_key(redis_key)
-		end
+    def initialize(redis_key)
+      @redis_key = format_redis_key(redis_key)
+    end
 
-		def self.flushdb
-			connection.flushdb
-		end
+    def self.flushdb
+      connection.flushdb
+    end
 
-		@@namespace = nil
-		@@connection = nil
+    @@namespace = nil
+    @@connection = nil
     def self.configure(options)
-    	@@namespace = options[:namespace]
+      @@namespace = options[:namespace]
       @@connection = options[:connection]
     end
 
-		def self.connection
-			return @@connection
-		end
+    def self.connection
+      return @@connection
+    end
 
-		def connection
-			return @@connection
-		end
+    def connection
+      return @@connection
+    end
 
-		def namespace
-			return @@namespace
-		end
+    def namespace
+      return @@namespace
+    end
 
-		def delete
-			return connection.del(@redis_key)
-		end
+    def delete
+      return connection.del(@redis_key)
+    end
 
-		def type
-			return connection.type(@redis_key)
-		end
+    def type
+      return connection.type(@redis_key)
+    end
 
-		def expire(expiry)
-			return connection.expire(@redis_key, expiry)
-		end
+    def expire(expiry)
+      return connection.expire(@redis_key, expiry)
+    end
 
-		def expireat(timestamp)
-			return connection.expireat(@redis_key, timestamp.to_i)
-		end
+    def expireat(timestamp)
+      return connection.expireat(@redis_key, timestamp.to_i)
+    end
 
-		def dump
-			return connection.dump(@redis_key)
-		end
+    def dump
+      return connection.dump(@redis_key)
+    end
 
-		def exists?
-			return connection.exists(@redis_key)
-		end
+    def exists?
+      return connection.exists(@redis_key)
+    end
 
-		def persist
-			return connection.persist(@redis_key)
-		end
+    def persist
+      return connection.persist(@redis_key)
+    end
 
-		def ttl
-			return connection.ttl(@redis_key)
-		end
+    def ttl
+      return connection.ttl(@redis_key)
+    end
 
-		def pttl
-			return connection.pttl(@redis_key)
-		end
+    def pttl
+      return connection.pttl(@redis_key)
+    end
 
-		def format_redis_key(key)
-			key = "#{namespace}:#{key}" unless key.starts_with?(namespace)
-			return key
-		end
-		private :format_redis_key
-	end
+    def format_redis_key(key)
+      key = "#{namespace}:#{key}" unless key.starts_with?(namespace)
+      return key
+    end
+    private :format_redis_key
+  end
 end
