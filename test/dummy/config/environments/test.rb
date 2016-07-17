03e88cabd917e5be3a5317c18118318ed19b77a6
Sergio Medina <medina@checkitmobile.com>
2016-07-17 11:38:01 +0200

    run tests in random order

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.active_support.test_order = :random
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
@@ -13,7 +15,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Configure static asset server for tests with Cache-Control for performance.
-  config.serve_static_files  = true
+  config.serve_static_files = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Show full error reports and disable caching.
